75303c652fed1b9e38c31bbf130c81d8dc66b869
David De Anda <omarandstuff@gmail.com>
2024-03-11 23:58:26 -0600

    Prepar development with presenter

--- src/runWorkflow.ts
+++ src/runWorkflow.ts
@@ -1,10 +1,27 @@
-import { TerminalPresenter, Workflow } from '@universal-packages/workflows'
+import { TerminalPresenter } from '@universal-packages/terminal-presenter'
+import { Workflow } from '@universal-packages/workflows'
+import { WorkflowTerminalPresenter } from '@universal-packages/workflows-terminal-presenter'
 
 export async function runWorkflow(name: string): Promise<void> {
-  const workflow = Workflow.buildFrom(name)
-  const terminalPresenter = new TerminalPresenter(workflow)
+  TerminalPresenter.start()
 
-  terminalPresenter.present()
+  const workflow = Workflow.buildFrom(name)
+  const workflowTerminalPresenter = new WorkflowTerminalPresenter({
+    showStrategyRoutines: 'running',
+    workflow
+  })
+
+  process.addListener('SIGINT', () => {
+    if (workflow.status === 'stopping') {
+      workflow.stop()
+    } else {
+      process.exit(0)
+    }
+  })
+
+  workflowTerminalPresenter.present()
 
   await workflow.run()
+
+  TerminalPresenter.stop()
 }
