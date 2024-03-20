11d9a0c4104ff78652bed238113b2ac6a875fa14
David De Anda <omarandstuff@gmail.com>
2024-03-20 16:14:54 -0600

    Run workflows from within src

--- src/runWorkflow.ts
+++ src/runWorkflow.ts
@@ -1,11 +1,12 @@
 import { TerminalPresenter } from '@universal-packages/terminal-presenter'
 import { Workflow } from '@universal-packages/workflows'
 import { WorkflowTerminalPresenter } from '@universal-packages/workflows-terminal-presenter'
+import path from 'path'
 
 export async function runWorkflow(name: string, variables?: Record<string, any>): Promise<void> {
   TerminalPresenter.start()
 
-  const workflow = Workflow.buildFrom(name, { variables })
+  const workflow = Workflow.buildFrom(name, { variables, workflowsLocation: path.resolve(__dirname) })
   const workflowTerminalPresenter = new WorkflowTerminalPresenter({
     showStrategyRoutines: 'running',
     workflow
