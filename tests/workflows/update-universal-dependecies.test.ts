b1ce2c2cfad2143e5be2a36c64d113adc79fae7f
David De Anda <omarandstuff@gmail.com>
2024-03-22 17:01:24 -0600

    Check tests after update

--- tests/workflows/update-universal-dependecies.test.ts
+++ tests/workflows/update-universal-dependecies.test.ts
@@ -12,6 +12,7 @@ describe('update-universal-dependencies', (): void => {
       { command: 'npm update @universal-packages/workflows --save' },
       { command: 'npm update @universal-packages/workflows-terminal-presenter --save' },
       { command: 'npm update @universal-packages/workflows-jest --save-dev' },
+      { command: 'npm run test:full' },
       { command: 'git status' },
       { command: 'git add .' },
       { command: 'git commit -m "Bump @universal-packages dependencies"' },
