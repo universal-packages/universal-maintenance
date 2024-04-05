6e928d34fa29b30427f257a577b27d9c5212e398
David De Anda <omarandstuff@gmail.com>
2024-04-05 14:56:10 -0600

    Update to new terminal presenter usage

--- src/runWorkflow.ts
+++ src/runWorkflow.ts
@@ -1,16 +1,14 @@
 import { Logger } from '@universal-packages/logger'
 import { TerminalPresenterTransport } from '@universal-packages/logger-terminal-presenter'
-import { TerminalPresenter } from '@universal-packages/terminal-presenter'
+import { present, restore } from '@universal-packages/terminal-presenter'
 import { Status, Workflow } from '@universal-packages/workflows'
 import { WorkflowTerminalPresenter } from '@universal-packages/workflows-terminal-presenter'
 
 export async function runWorkflow(name: string, variables?: Record<string, any>): Promise<void> {
-  const terminalPresenter = new TerminalPresenter()
-
-  terminalPresenter.present()
-
   const logger = new Logger({ includeTransportAdapters: { 'terminal-presenter': TerminalPresenterTransport }, transports: ['terminal-presenter'] })
   await logger.prepare()
+
+  present()
 
   const workflow = Workflow.buildFrom(name, { variables, workflowsLocation: __dirname, stepUsableLocation: __dirname })
   const workflowTerminalPresenter = new WorkflowTerminalPresenter({ logger, showStrategyRoutines: 'running', workflow })
@@ -27,7 +25,7 @@ export async function runWorkflow(name: string, variables?: Record<string, any>)
 
   await workflow.run()
 
-  await terminalPresenter.restore()
+  await restore()
 
   if ([Status.Error, Status.Failure].includes(workflow.status)) {
     process.exit(1)
